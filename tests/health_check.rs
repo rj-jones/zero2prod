87fef4de0171a03443cbf1e413ed149bc6051c86
rj-jones <rjjones@fastmail.com>
2023-06-26 14:40:55 -0500

    upgrade health_check test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 // [X] is exposed at /health_check;
 // [X] is behind a GET method;
 // [X] check always returns a 200;
@@ -5,12 +7,12 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -23,7 +25,10 @@ async fn health_check_works() {
 // Launch the server as a background task
 // tokio::spawn returns a handle to the spawned future,
 // but we have no use for it here, hence the non-binding let
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
